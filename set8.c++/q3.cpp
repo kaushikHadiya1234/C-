dda983104ba1e31a5bb1d49879c742557a5cecdb
kaushikHadiya1234 <119835333+kaushikHadiya1234@users.noreply.github.com>
2023-01-16 11:04:07 +0530

    Add files via upload

--- set8.c++/q3.cpp
+++ set8.c++/q3.cpp
@@ -5,26 +5,57 @@
 using namespace std;
 class A
 {
+	private :
+		
+		int no;
+		
 	public :
 		
 		void set()
 		{
-			cout<<"hadiya ";
+			cout<<"enter the value of any number=>>"<<endl;
+			cin>>no;
+			
+			if(no%2==0)
+			{
+				cout<<"your number is even"<<endl;
+			}
+			else
+			{
+				cout<<"your number is odd"<<endl;
+			}
+			
+			cout<<"\n===============================\n";
 		}
 };
 class B
 {
+	private :
+		
+		int year;
+		
 	public :
 		void set()
 		{
-			cout<<"kaushik"<<endl;
+			cout<<"enter the  any year;"<<endl;
+			cin>>year;
+			
+			if(year%4==0)
+			{
+				cout<<"your year is leap year"<<endl;
+			}
+			else
+			{
+				cout<<"your year is not leap year"<<endl;
+			}
+			
 		}
 };
 main()
 {
 	A a1;
-	B b1;
-	
 	a1.set();
+	
+	B b1;
 	b1.set();
 }
